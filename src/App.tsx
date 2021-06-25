e912373d9fdf8586ffaebcc64f182169146f5586
Raian Soares <raiansoares72@gmail.com>
2021-06-25 17:44:07 -0300

    Update version

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { NewRoon } from './pages/NewRoon';
+
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
+
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoon} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
